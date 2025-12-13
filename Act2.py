c1c5b50b797596f0113145aefc7172d9df2fd8be
Taqiyya1 <142751674+Taqiyya1@users.noreply.github.com>
2025-12-12 19:57:39 -0500

    Add files via upload

---
+++ Act2.py
@@ -0,0 +1,12 @@
+#Write a program to create different variables to store various values.
+
+x = 5
+y = "John"
+print(x)
+print(y)
+
+codingal = "codingal"
+print("codingal")
+name = input("enter your name: ")
+
+print("\nHello", name, "\nwelcome to codingal")
