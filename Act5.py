d6a8b5b059ed8c74b4b75238b429d249e49ab6ec
Taqiyya1 <142751674+Taqiyya1@users.noreply.github.com>
2025-12-19 19:19:48 -0500

    Add files via upload

---
+++ Act5.py
@@ -0,0 +1,11 @@
+#What is the average height of a tree in Jack's front yard?
+tree1 = 98
+tree2 = 94
+tree3 = 41
+tree4 = 95
+tree5 = 11
+
+sum = 1+2+3+4+5
+print("the sum of all the trees is: ", sum)
+average = sum/5
+print("The average of all the trees is: ", average)
