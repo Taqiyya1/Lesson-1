c64b7354d233ea1a9c5979da37c920b7d6401b36
Taqiyya1 <142751674+Taqiyya1@users.noreply.github.com>
2026-01-04 18:34:16 -0500

    Add files via upload

---
+++ Mean_Value.py
@@ -0,0 +1,15 @@
+# Find the correct mean of given numbers.
+
+mean1 = 38
+wrong_number = 36
+correct_number = 56
+total_numbers = 40
+
+sum = mean1*total_numbers
+print("The sum of the 40 numbers are ", sum)
+
+num2 = sum-((wrong_number)-(correct_number))
+print("sum-((wrong_number)-(correct_number)): ", num2)
+
+mean2 = num2/total_numbers
+print(mean2)
