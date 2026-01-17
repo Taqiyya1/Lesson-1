f1693f3f482440116a85b65e88cb5687509e538c
Taqiyya1 <142751674+Taqiyya1@users.noreply.github.com>
2026-01-16 19:25:34 -0500

    Add files via upload

---
+++ Prime_Number.py
@@ -0,0 +1,14 @@
+# Write a program to print all the prime numbers which come in the range entered by the user?
+
+lower = int(input("Enter a lower range: "))
+upper = int(input("Enter a upper range: "))
+
+print("Prime numbers between", lower, "and", upper, "are: ")
+
+for num in range(lower, upper + 1):
+    if num > 1:
+        for i in range(2, num):
+            if (num % i) == 0:
+                break
+        else:
+            print(num)
