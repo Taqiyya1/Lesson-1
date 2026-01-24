bf56c932a5fc28c1d8d96720c465dd8c27a71549
Taqiyya1 <142751674+Taqiyya1@users.noreply.github.com>
2026-01-23 19:18:39 -0500

    Add files via upload

---
+++ weather_condition.py
@@ -0,0 +1,9 @@
+# Write a program to display the weather in autumn and spring :
+
+def weather_conditions():
+    print("Weather is pleasant in:", spring)
+    print("the weather is same in", autumn)
+    
+spring = "autumn"
+autumn = spring
+weather_conditions()
