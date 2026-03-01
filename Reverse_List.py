fef3f091b9c68046dfe7ee09e5576b86e41756e0
Taqiyya1 <142751674+Taqiyya1@users.noreply.github.com>
2026-03-01 16:13:07 -0500

    Add files via upload

---
+++ Reverse_List.py
@@ -0,0 +1,18 @@
+# Write a program to perform the following operations on a List: 
+# 1.Create an empty list 
+# 2.A list with elements 
+# 3.Use * operator 
+# 4.Reverse a list
+
+empty_list = []
+print()
+
+numbers = [1,2,3,4,5]
+print(numbers)
+
+triples = [1,2,3]*3
+print(triples)
+
+aList = [100,200,300,400,500]
+aList = aList[::-1]
+print(aList,"\n")
