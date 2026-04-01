cb0ed093f7f1354d844a697e45cea7d8145e489e
Taqiyya1 <142751674+Taqiyya1@users.noreply.github.com>
2026-03-31 20:20:37 -0400

    Add files via upload

---
+++ array.py
@@ -0,0 +1,13 @@
+# Write a program to create an array with the following elements - [1, 3, 5, 3, 7, 9, 3]. 
+# Then find the number of occurrences of number 3 in the array. Also, print the reversed array.
+
+import array as arr
+
+array_num = arr.array('i', [1, 3, 5, 3, 7, 9, 3])
+print("Original array: "+str(array_num))
+
+print("Number of occurances of the number 3 in the said array: "+str(array_num.count(3)))
+
+array_num.reverse()
+print("reverse the order of the items:")
+print(str(array_num))
