de593a96f3df3df5a479499809db21838488882c
Taqiyya1 <142751674+Taqiyya1@users.noreply.github.com>
2026-04-04 18:11:52 -0400

    Add files via upload

---
+++ Class_Vehicle.py
@@ -0,0 +1,14 @@
+# Write a program to create a class Vehicle and perform the following tasks:
+# 1. Create an __init__ method with arguments - max_speed and mileage 
+# 2. Create an object of class Vehicle and pass the maximum speed and mileage of the car 
+# 3. Print the values of max_speed and mileage by using the object
+
+class Vehicle:
+    def __init__(self, max_speed, mileage):
+        self.max_speed = max_speed
+        self. mileage = mileage
+        
+modelX = Vehicle(240, 18)
+
+print("Model Max Speed:", modelX.max_speed)
+print("Model Mileage:", modelX.mileage)
