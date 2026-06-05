0cffa8c705af5afc050775ef0195930b2fef4325
Taqiyya1 <142751674+Taqiyya1@users.noreply.github.com>
2026-06-04 21:06:40 -0400

    Add files via upload

---
+++ Widgets.py
@@ -0,0 +1,39 @@
+# Create an interactive GUI application using Tkinter that greets users by name and 
+# displays today's date! Students will learn to build windows, add widgets, 
+# handle button clicks, and display dynamic messages in a text box.
+
+from email import message
+from tkinter import *
+from datetime import date
+
+root = Tk()
+root.title('Getting Started With Widgets')
+root.geometry('400x300')
+
+lbl = Label(text = "Hey there!", fg = "white", bg = "#072F5F", height = 1, width = 300)
+
+name_lbl = Label(text = "Full name", bg = "#3895D3")
+name_entry = Entry()
+
+def display():
+    name = name_entry.get()
+    
+    global message
+    message = "Welcome to the Application! \nToday's date is: "
+    greet = "Hello "+name+"\n"
+    text_box.insert(END, greet)
+    text_box.insert(END, message)
+    text_box.insert(END, date.today)
+    
+text_box = Text(height = 3)
+
+btn = Button(text = "Begin", command = display, height = 1, bg = "#1261A0", fg = 'white')
+
+lbl.pack()
+name_lbl.pack()
+name_entry.pack()
+btn.pack()
+text_box.pack()
+
+root.mainloop()
+
